327748f7a2cb9abcc45ebc90d7acd9b493557b65
kiko-g <kikojpgoncalves@gmail.com>
2024-10-17 01:45:08 +0100

    delete badge imports

--- src/components/showcase/application-ui/badges/index.ts
+++
@@ -1,7 +0,0 @@
-import { BadgeBeta } from './BadgeBeta'
-import { BadgeError } from './BadgeError'
-import { BadgeSimple } from './BadgeSimple'
-import { BadgeSuccess } from './BadgeSuccess'
-import { BadgeOutline } from './BadgeOutline'
-
-export { BadgeError, BadgeSimple, BadgeSuccess, BadgeOutline }
