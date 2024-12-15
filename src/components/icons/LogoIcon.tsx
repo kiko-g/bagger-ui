d4145d5f443054df243e1f31298313110eac7f13
kiko-g <kikojpgoncalves@gmail.com>
2024-12-15 00:40:38 +0000

    new favicon

--- src/components/icons/LogoIcon.tsx
+++ src/components/icons/LogoIcon.tsx
@@ -4,9 +4,64 @@ import React from 'react'
 
 export function LogoIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
+    <svg width="140" height="140" viewBox="0 0 140 140" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <g opacity={0.9}>
+        <rect
+          width={45.255}
+          height={45.255}
+          x={37.99}
+          y={107.632}
+          fillOpacity={0.9}
+          strokeWidth={2}
+          strokeOpacity={0.0}
+          rx={10}
+          transform="rotate(-45 37.99 107.632)"
+          className="fill-zinc-950 stroke-zinc-950 dark:fill-zinc-50 dark:stroke-zinc-50"
+        />
+      </g>
+      <path
+        fillOpacity={0.9}
+        strokeWidth={2}
+        strokeOpacity={0.0}
+        d="M45.768 25.99 63.626 8.134a9 9 0 0 1 12.728 0L94.212 25.99a9 9 0 0 1 0 12.727L76.354 56.576a9 9 0 0 1-12.728 0L45.768 38.718a9 9 0 0 1 0-12.728Z"
+        opacity={0.8}
+        className="fill-zinc-950 stroke-zinc-950 dark:fill-zinc-50 dark:stroke-zinc-50"
+      />
+      <rect
+        width={43.255}
+        height={43.255}
+        rx={9}
+        x={77.043}
+        y={69.993}
+        fillOpacity={0.9}
+        strokeWidth={2}
+        strokeOpacity={0.0}
+        opacity={0.8}
+        transform="rotate(-45 77.043 69.993)"
+        className="fill-zinc-950 stroke-zinc-950 dark:fill-zinc-50 dark:stroke-zinc-50"
+      />
+      <rect
+        width={43.255}
+        height={43.255}
+        x={1.766}
+        y={69.993}
+        fillOpacity={0.9}
+        strokeWidth={2}
+        strokeOpacity={0.0}
+        opacity={0.8}
+        rx={9}
+        transform="rotate(-45 1.766 69.993)"
+        className="fill-zinc-950 stroke-zinc-950 dark:fill-zinc-50 dark:stroke-zinc-50"
+      />
+    </svg>
+  )
+}
+
+export function LogoIconOld(props: React.SVGProps<SVGSVGElement>) {
+  return (
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="16 16 42 42" {...props}>
       <path fill="url(#a)" d="M18.5 36c0-9.941 8.059-18 18-18s18 8.059 18 18-8.059 18-18 18-18-8.059-18-18Z" />
-      <g opacity={0.8}>
+      <g opacity={0.9}>
         <rect
           width={9}
           height={9}
@@ -23,7 +78,7 @@ export function LogoIcon(props: React.SVGProps<SVGSVGElement>) {
           x={30.283}
           y={43.485}
           stroke="#fff"
-          strokeOpacity={0.8}
+          strokeOpacity={1.0}
           strokeWidth={0.5}
           rx={1.75}
           transform="rotate(-45 30.283 43.485)"
--- src/components/layout/LogoLink.tsx
+++ src/components/layout/LogoLink.tsx
@@ -6,7 +6,7 @@ import { LogoIcon } from '@/components/icons/LogoIcon'
 export function LogoLink() {
   return (
     <Link href="/" className="flex items-center justify-center gap-1.5 transition hover:opacity-80">
-      <LogoIcon className="mt-[2px] h-7 w-7" />
+      <LogoIcon className="h-6 w-6" />
       <span className="hidden font-extrabold lg:inline-block">Bagger UI</span>
     </Link>
   )
